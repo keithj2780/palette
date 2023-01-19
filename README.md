70ff405da3c876500999597326bd5395f38c1202
keithj2780 <keithj2780@gmail.com>
2023-01-19 17:42:34 +1100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+---
+
+# svelte colour palette picker with HSV editor
+
+
+Demo at [REPL](https://svelte.dev/repl/2a73898419014e96a607d070635445df?version=3.55.1)
+
+No dependencies.  
+
+This is two component in one package.  
+
+1. A HSV colour picker supporting HSV and alpha.  
+
+2. A Palette picker for preset colours. These colours can be edited by the end user using the HSV colour picker above.  
+
+dispatches 'input' and 'change' messages -  REPL for example.
---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	let name = 'world';
+	import HSVColourPicker from './HSVColourPicker.svelte';
+	import ColourPicker from './ColourPicker.svelte';
+	
+	function colorChange(e) {
+		console.log('change',e.detail);
+	}
+	function colorInput(e) {
+		console.log('input',e.detail);
+	}
+</script>
+
+<h1>HSV Colour Picker</h1>
+
+<HSVColourPicker startColor={"#000000"} on:change={colorChange} on:input={colorInput} showAlphaSlider={true}
+    showInfoBox={true}/>
+
+
+<h1>Palette editable using HSV Colour Picker</h1>
+
+<ColourPicker on:input={colorInput}/>
